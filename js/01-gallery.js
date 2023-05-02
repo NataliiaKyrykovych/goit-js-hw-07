03bec3ea459f600eda29e6a5c6b4ae56902729d2
NataliiaKyrykovych <120452030+NataliiaKyrykovych@users.noreply.github.com>
2023-05-02 10:35:56 -0500

    Update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const imageList = document.querySelector('.gallery');
+const imageListItem = createGalleryImageList(galleryItems);
+
+imageList.addEventListener('click', onImageListClick)
+
+function onImageListClick(event) {
+    console.log(event.target);
+}
+
+
+
 
 console.log(galleryItems);
